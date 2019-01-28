4ed6d8d01915d9f8e722c13c5a0cda4e18df1a7e
aik1979 <46378699+aik1979@users.noreply.github.com>
2019-01-28 09:44:18 +0000

    initial commit

---
+++ precache-manifest.d23eaa834d96934737c79543e6bd511e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f870f1c3eb743fb86e6",
+    "url": "/static/js/main.5f870f1c.chunk.js"
+  },
+  {
+    "revision": "fa95d02d532f002b3814",
+    "url": "/static/js/1.fa95d02d.chunk.js"
+  },
+  {
+    "revision": "5f870f1c3eb743fb86e6",
+    "url": "/static/css/main.2438d86c.chunk.css"
+  },
+  {
+    "revision": "fa95d02d532f002b3814",
+    "url": "/static/css/1.4a1a6af7.chunk.css"
+  },
+  {
+    "revision": "6e5ff7d5d2d1646a209f6c30f01772d6",
+    "url": "/index.html"
+  }
+];
